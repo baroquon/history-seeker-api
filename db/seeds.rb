f2a68749b84567c238c4220cb9b3ac9df98cc182
Brandon Blaylock <brandon.blaylock@gmail.com>
2015-05-20 23:39:43 -0500

    added some civil war seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,88 @@
-teacher = User.create(first_name: 'Brandon', middle_name: 'Everett', last_name: 'Blaylock', role: 'teacher', date_of_birth: '10/09/1983', email: 'brandon@isotope11.com', password: 'password')
-User.create(first_name: 'Josh', middle_name: 'Sloan', last_name: 'Adams', role: 'student', teacher: teacher, date_of_birth: '02/14/1983', email: 'josh@isotope11.com', password: 'password')
-User.create(first_name: 'Kristen', middle_name: 'Joy', last_name: 'Adams', role: 'student', teacher: teacher, date_of_birth: '09/17/1982', email: 'kristen@isotope11.com', password: 'password')
-User.create(first_name: 'Sarah', middle_name: 'Elaine', last_name: 'Blaylock', role: 'student', teacher: teacher, date_of_birth: '09/24/1984', email: 'sarah@isotope11.com', password: 'password')
-User.create(first_name: 'Brett', middle_name: 'Matthew', last_name: 'Blaylock', role: 'student', teacher: teacher, date_of_birth: '09/02/1988', email: 'brett@isotope11.com', password: 'password')
-User.create(first_name: 'Greg', middle_name: 'Buck', last_name: 'Kinnear', role: 'teacher', date_of_birth: '06/17/1963', email: 'greg@isotope11.com', password: 'password')
-
-Fact.create(title: 'Green Zone is Released', description: 'Staring the incomparable Greg Kinnear the movie Green Zone was release March 11, 2010', start_date: '3/11/2010', end_date: '3/11/2010')
+#teacher = User.create(first_name: 'Brandon', middle_name: 'Everett', last_name: 'Blaylock', role: 'teacher', date_of_birth: '10/09/1983', email: 'brandon@isotope11.com', password: 'password')
+#User.create(first_name: 'Josh', middle_name: 'Sloan', last_name: 'Adams', role: 'student', teacher: teacher, date_of_birth: '02/14/1983', email: 'josh@isotope11.com', password: 'password')
+#User.create(first_name: 'Kristen', middle_name: 'Joy', last_name: 'Adams', role: 'student', teacher: teacher, date_of_birth: '09/17/1982', email: 'kristen@isotope11.com', password: 'password')
+#User.create(first_name: 'Sarah', middle_name: 'Elaine', last_name: 'Blaylock', role: 'student', teacher: teacher, date_of_birth: '09/24/1984', email: 'sarah@isotope11.com', password: 'password')
+#User.create(first_name: 'Brett', middle_name: 'Matthew', last_name: 'Blaylock', role: 'student', teacher: teacher, date_of_birth: '09/02/1988', email: 'brett@isotope11.com', password: 'password')
+#User.create(first_name: 'Greg', middle_name: 'Buck', last_name: 'Kinnear', role: 'teacher', date_of_birth: '06/17/1963', email: 'greg@isotope11.com', password: 'password')
+#
+Fact.create(
+  title: 'Abraham Lincoln Elected President',
+  description: 'Lincoln won the Electoral College with less than 40 percent of the popular vote nationwide by carrying states above the Mason–Dixon line and north of the Ohio River, plus the far west California and Oregon.',
+  start_date: '6/11/1860',
+  end_date: '6/11/1860',
+  lat: '39.8016667',
+  lng: '-89.6436111',
+  addditional_info_link: 'https://www.whitehouse.gov/1600/presidents/abrahamlincoln'
+)
+Fact.create(
+  title: 'South Carolina secedes from the Union.',
+  description: 'On December 20, 1860, by a vote of 169-0, the South Carolina legislature enacted an "ordinance" that "the union now subsisting between South Carolina and other States, under the name of &apos;The United States of America,&apos; is hereby dissolved."',
+  start_date: '20/12/1860',
+  end_date: '20/12/1860',
+  lat: '34.0298',
+  lng: '-80.8966',
+  addditional_info_link: 'http://www.ushistory.org/us/32e.asp'
+)
+Fact.create(
+  title: 'The Confederate States of America is formed',
+  description: 'The Confederate States of America is formed with Jefferson Davis as president and Alexander Stephens vice president. The founding members were South Carolina, Mississippi, Florida, Alabama, Georgia, Louisiana, and Texas. They would later be joined by Virginia, Arkansas, Tennessee, and North Carolina.',
+  start_date: '09/02/1861',
+  end_date: '09/02/1861',
+  lat: '32.3617',
+  lng: '-86.2792',
+  addditional_info_link: 'http://www.history.com/topics/american-civil-war/confederate-states-of-america'
+)
+Fact.create(
+  title: 'Abraham Lincoln is sworn in as 16th President of the United States of America.',
+  description: '"Apprehension seems to exist among the people of the Southern States that by the accession of a Republican Administration their property and their peace and personal security are to be endangered. There has never been any reasonable cause for such apprehension. Indeed, the most ample evidence to the contrary has all the while existed and been open to their inspection. It is found in nearly all the published speeches of him who now addresses you. I do but quote from one of those speeches when I declare that — I have no purpose, directly or indirectly, to interfere with the institution of slavery in the States where it exists. I believe I have no lawful right to do so, and I have no inclination to do so." - Abraham Lincoln, Inaugural Address',
+  start_date: '04/03/1861',
+  end_date: '04/03/1861',
+  lat: '38.9047',
+  lng: '-77.0164',
+  addditional_info_link: 'https://www.whitehouse.gov/1600/presidents/abrahamlincoln'
+)
+Fact.create(
+  title: 'Battle of Fort Sumter',
+  description: 'Following declarations of secession by seven Southern states, South Carolina demanded that the US Army abandon its facilities in Charleston Harbor. The Federals refusal to leave the fort. The Battle of Fort Sumter involved the bombardment and surrender of Fort Sumter, near Charleston, South Carolina, that started the American Civil War. The Confederate forces were led by General P.G.T. Beauregard.',
+  start_date: '12/04/1861',
+  end_date: '14/04/1861',
+  lat: '32.783',
+  lng: '-79.9333',
+  addditional_info_link: 'http://www.civilwar.org/battlefields/fort-sumter.html'
+)
+Fact.create(
+  title: 'Robert E. Lee, former Superintendent of West Point, is offered command of the Union Army.',
+  description: 'Lee declines Lincoln&apos;s offer to lead the Union Army.',
+  start_date: '15/04/1861',
+  end_date: '15/04/1861',
+  lat: '38.9047',
+  lng: '-77.0164',
+  addditional_info_link: 'http://www.civilwar.org/education/history/biographies/robert-e-lee.html'
+)
+Fact.create(
+  title: 'President Lincoln begins a blockade against Southern ports.',
+  description: 'U.S. Secretary of State William Henry Seward recommended adopting the blockade shortly after the Battle of Fort Sumter. For the duration of the war the blockade limits the ability of the rural South to stay well supplied in its war against the industrialized North. The blockades also allowed foreign governments to grant the Confederacy belligerent status.',
+  start_date: '19/04/1861',
+  end_date: '19/04/1861',
+  lat: '29.9500',
+  lng: '-90.0667',
+  addditional_info_link: 'https://history.state.gov/milestones/1861-1865/blockade'
+)
+Fact.create(
+  title: 'Robert E. Lee resigns his commission in the United States Army.',
+  description: 'Lee then goes to Richmond, Virginia, is offered command of the military and naval forces of Virginia, and accepts.',
+  start_date: '20/04/1861',
+  end_date: '20/04/1861',
+  lat: '37.5333',
+  lng: '-77.4667',
+  addditional_info_link: 'http://www.civilwar.org/education/history/biographies/robert-e-lee.html'
+)
+Fact.create(
+  title: 'First Battle of Bull Run or First Manassas',
+  description: 'This was the first major land battle of the armies in Virginia. Here the Confederate Army defeated the Federal and General Thomas Jackson earned the nickname "Stonewall."',
+  start_date: '21/07/1861',
+  end_date: '21/07/1861',
+  lat: '38.7514',
+  lng: '-77.4764',
+  addditional_info_link: 'http://www.civilwar.org/battlefields/bullrun.html'
+)
