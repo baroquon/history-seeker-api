3744c0a0d364f2059068c96372315ae8877dbcd0
Brandon Blaylock <brandon.blaylock@gmail.com>
2015-05-21 15:30:55 -0500

    added email to the users serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :middle_name, :last_name, :role, :notes, :date_of_birth, :teacher_id, :student_ids, :curriculum_ids
+  attributes :id, :first_name, :middle_name, :last_name, :role, :notes, :email, :date_of_birth, :teacher_id, :student_ids, :curriculum_ids
 end
