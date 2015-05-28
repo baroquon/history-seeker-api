c94f3263b392a5e8570f07fd0f80b2e4034bf5b7
Brandon Blaylock <brandon.blaylock@gmail.com>
2015-05-27 20:00:59 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-#teacher = User.create(first_name: 'Brandon', middle_name: 'Everett', last_name: 'Blaylock', role: 'teacher', date_of_birth: '10/09/1983', email: 'brandon@isotope11.com', password: 'password')
-#User.create(first_name: 'Josh', middle_name: 'Sloan', last_name: 'Adams', role: 'student', teacher: teacher, date_of_birth: '02/14/1983', email: 'josh@isotope11.com', password: 'password')
-#User.create(first_name: 'Kristen', middle_name: 'Joy', last_name: 'Adams', role: 'student', teacher: teacher, date_of_birth: '09/17/1982', email: 'kristen@isotope11.com', password: 'password')
-#User.create(first_name: 'Sarah', middle_name: 'Elaine', last_name: 'Blaylock', role: 'student', teacher: teacher, date_of_birth: '09/24/1984', email: 'sarah@isotope11.com', password: 'password')
-#User.create(first_name: 'Brett', middle_name: 'Matthew', last_name: 'Blaylock', role: 'student', teacher: teacher, date_of_birth: '09/02/1988', email: 'brett@isotope11.com', password: 'password')
-#User.create(first_name: 'Greg', middle_name: 'Buck', last_name: 'Kinnear', role: 'teacher', date_of_birth: '06/17/1963', email: 'greg@isotope11.com', password: 'password')
-#
+teacher = User.create(first_name: 'Brandon', middle_name: 'Everett', last_name: 'Blaylock', role: 'teacher', date_of_birth: '10/09/1983', email: 'brandon@isotope11.com', password: 'password')
+User.create(first_name: 'Josh', middle_name: 'Sloan', last_name: 'Adams', role: 'student', teacher: teacher, date_of_birth: '02/14/1983', email: 'josh@isotope11.com', password: 'password')
+User.create(first_name: 'Kristen', middle_name: 'Joy', last_name: 'Adams', role: 'student', teacher: teacher, date_of_birth: '09/17/1982', email: 'kristen@isotope11.com', password: 'password')
+User.create(first_name: 'Sarah', middle_name: 'Elaine', last_name: 'Blaylock', role: 'student', teacher: teacher, date_of_birth: '09/24/1984', email: 'sarah@isotope11.com', password: 'password')
+User.create(first_name: 'Brett', middle_name: 'Matthew', last_name: 'Blaylock', role: 'student', teacher: teacher, date_of_birth: '09/02/1988', email: 'brett@isotope11.com', password: 'password')
+User.create(first_name: 'Greg', middle_name: 'Buck', last_name: 'Kinnear', role: 'teacher', date_of_birth: '06/17/1963', email: 'greg@isotope11.com', password: 'password')
+
 Fact.create(
   title: 'Abraham Lincoln Elected President',
   description: 'Lincoln won the Electoral College with less than 40 percent of the popular vote nationwide by carrying states above the Mason–Dixon line and north of the Ohio River, plus the far west California and Oregon.',
